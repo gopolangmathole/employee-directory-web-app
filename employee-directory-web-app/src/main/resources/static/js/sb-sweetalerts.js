88c654c8a60acca20c53028f19d5d90b79f28af7
Gopolang <Gopolang@Lenovo-PC>
2020-02-06 22:17:26 +0200

    Sweet alerts update

--- employee-directory-web-app/src/main/resources/static/js/sb-sweetalerts.js
+++ employee-directory-web-app/src/main/resources/static/js/sb-sweetalerts.js
@@ -3,14 +3,33 @@ document.getElementById("pdf").addEventListener("click", showPopUp);
 document.getElementById("html").addEventListener("click", showPopUp);
 
 // create my alert function
-function showPopUp() {
-
+function showPopUp() {	
+	let timerInterval
 	Swal.fire({
-		position : 'center',
-		icon : 'success',
-		title : 'Your report was successfully generated!',
-		showConfirmButton : false,
-		timer : 3200
-	});
+	  title: 'Generating Report',
+	  html: 'Please wait as we prepare your file!.',
+	  timer: 90000,
+	  timerProgressBar: true,
+	  onBeforeOpen: () => {
+	    Swal.showLoading()
+	    timerInterval = setInterval(() => {
+	      const content = Swal.getContent()
+	      if (content) {
+	        const b = content.querySelector('b')
+	        if (b) {
+	          b.textContent = Swal.getTimerLeft()
+	        }
+	      }
+	    }, 100)
+	  },
+	  onClose: () => {
+	    clearInterval(timerInterval)
+	  }
+	}).then((result) => {
+	  /* Read more about handling dismissals below */
+	  if (result.dismiss === Swal.DismissReason.timer) {
+	    console.log('I was closed by the timer')
+	  }
+	})
 	
 }
